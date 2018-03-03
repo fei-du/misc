198270de1c4877a92e1ff2c9a97da341f87b583a
Fei Du <fei.du@nxp.com>
2018-03-03 10:20:33 +0800

    nm jl hex nodemon for script execution

--- misc.psm1
+++ misc.psm1
@@ -7,4 +7,51 @@ function get-bits($base, $startBit, $endBit)
      "$base[$endBit" + ":"+ "$startBit" + "] = $value"
 }
 
-export-modulemember -function get-bits
+
+function com {
+    [System.IO.Ports.SerialPort]::GetPortNames()
+}
+
+function Hex($decimal, $base=16) {
+    [convert]::ToString($decimal, $base)
+}
+
+function jl{
+    JLink.exe -new_console:s
+}
+
+function nm($file, [switch]$v){
+	$ex = gci $file | select -ExpandProperty extension
+	if($v){
+	    $split = '-new_console:s40V'
+	} else{
+	    $split = '-new_console:s40H'
+	}
+	switch($ex){
+		".ps1" {
+		    nodemon $split --exec 'C:\windows\System32\WindowsPowerShell\v1.0\powershell.exe -noprofile' $file   --ext ps1
+		    break
+		    }
+		".R"{
+		    # somehow not working when updating node from v6 to v8
+
+		    $env:Path += ";C:\Program Files\R\R-3.4.0\bin\"
+		    nodemon $split --exec 'Rscript.exe' $file   --ext R
+		    break
+		    }
+		# ".py"{
+		#     nodemon $split --exec 'python' $file   --ext py
+		#     break
+		# }
+		".jlk"{
+		    nodemon $split --exec 'jlink' $file   --ext jlk
+		    break
+		    }
+		".js"{
+			nodemon $split $file
+		    }
+		default {"not supported yet"}
+	    }
+}
+
+export-modulemember -function get-bits, com, hex, jl, nm
