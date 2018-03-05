78453f87a8470978cef7c27af72b5479ed67ba3a
Fei Du <fei.du@nxp.com>
2018-03-05 10:36:37 +0800

    get-bits output object to pipeline

--- misc.psm1
+++ misc.psm1
@@ -2,9 +2,12 @@ function get-bits($base, $startBit, $endBit)
 {
      if ( $endBit -eq $null) {$endBit = $startBit}
      $string = $input |  Select-String "^.{5}$base" 
-     $string
      $value = $string | ConvertFrom-String | % { ([convert]::ToString("0x$($_.p3)", 2)).padleft(32, '0').tochararray()[(31-$endBit)..(31-$startBit)] } | join-string
-     "$base[$endBit" + ":"+ "$startBit" + "] = $value"
+     @{
+     rawValue= $string
+     address = "$base[$endBit" + ":"+ "$startBit" + "]"
+     value = $value
+     }
 }
 
 
