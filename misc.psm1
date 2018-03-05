51b0b729df1e84dc9f476a754977406d0f87a605
Fei Du <fei.du@nxp.com>
2018-03-05 20:08:37 +0800

    previous was hashtable, now it's true psobject

--- misc.psm1
+++ misc.psm1
@@ -3,7 +3,7 @@ function get-bits($base, $startBit, $endBit)
      if ( $endBit -eq $null) {$endBit = $startBit}
      $string = $input |  Select-String "^.{5}$base" 
      $value = $string | ConvertFrom-String | % { ([convert]::ToString("0x$($_.p3)", 2)).padleft(32, '0').tochararray()[(31-$endBit)..(31-$startBit)] } | join-string
-     @{
+     [PSCustomObject] @{
      rawValue= $string
      address = "$base[$endBit" + ":"+ "$startBit" + "]"
      value = $value
